a7ef64567fe16d1eec851beadd13046e813c1ea9
joso <josip.mesaric@hotmail.com>
2018-10-30 11:55:22 +0100

    Removed environments folder

--- app/helpers/movies_populator.rb
+++ app/helpers/movies_populator.rb
@@ -5,14 +5,7 @@ require 'net/http'
 class MoviesPopulator
   def self.populate_movies
     (1..5).each do |page|
-      # todo gdje spremiti ovaj api kljuc?
-      url = URI("https://api.themoviedb.org/3/movie/top_rated?api_key=e95ef8f8eb154d4d0fb1ba5d47cb66fc&language=en-US&page=#{page}")
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      request = Net::HTTP::Get.new(url)
-      request.body = "{}"
-      response = http.request(request)
+      response = getAPIResponse(page)
 
       JSON.parse(response.read_body)['results'].select do |movie|
         new_movie = Movie.create('id': movie['id'],
@@ -21,6 +14,8 @@ class MoviesPopulator
                                  'overview': movie['overview'],
                                  'score': movie['vote_average'],
                                  'genre_id': movie['genre_ids'][0])
+
+
         new_movie.grab_image(File.join('https://image.tmdb.org/t/p/w500', movie['poster_path']),
                          movie['poster_path'][1..-1])
         puts "THIS MOVIE WAS ADDED TO DATABASE: ",
@@ -29,7 +24,7 @@ class MoviesPopulator
              movie['vote_average']
       end
     end
-    return
+    nil
   end
 
   def self.populate_genres
@@ -44,11 +39,35 @@ class MoviesPopulator
 
     response = http.request(request)
 
-    JSON.parse(response.read_body)['genres'].select do |genre|  #consider using map instead of select?
+    JSON.parse(response.read_body)['genres'].select do |genre|  # todo consider using map instead of select?
       Genre.create('name': genre['name'],
                    'id': genre['id'])
       puts genre['name'] + ' created'
     end
-    return
+    nil
+  end
+
+  private
+
+  def self.getAPIResponse(page)
+    # todo gdje spremiti ovaj api kljuc?
+    url = URI("https://api.themoviedb.org/3/movie/top_rated?api_key=e95ef8f8eb154d4d0fb1ba5d47cb66fc&language=en-US&page=#{page}")
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(url)
+    request.body = "{}"
+    http.request(request)
+  end
+
+  def self.populateActors(movieID)
+    url = URI("https://api.themoviedb.org/3/movie/#{movieID}?api_key=e95ef8f8eb154d4d0fb1ba5d47cb66fc&append_to_response=credits")
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(url)
+    request.body = "{}"
+    response = http.request(request)
+    return JSON.parse(response.read_body)['credits']['cast'][0 ... 10]
   end
 end
